ece9cddebdede60114ee43981fed1b6308d143b1
Mark Hambly <hambly@gmail.com>
2013-03-07 16:33:15 +1300

    Initial Commit

---
+++ BJCPStyles/BJCPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BJCPAppDelegate.h
+//  BJCPStyles
+//
+//  Created by Mark on 3/7/13.
+//  Copyright (c) 2013 Mark Hambly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BJCPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BJCPStyles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BJCPStyles
+//
+//  Created by Mark on 3/7/13.
+//  Copyright (c) 2013 Mark Hambly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BJCPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BJCPAppDelegate class]));
+    }
+}
